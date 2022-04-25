d2cee3932864c680817c049a46ba7f7de26f1d53
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-04-25 19:58:14 +0200

    initial commit

--- src/GenieDeployJuliaHub.jl
+++ src/GenieDeployJuliaHub.jl
@@ -1,5 +1,28 @@
 module GenieDeployJuliaHub
 
-# Write your package code here.
+import Genie
+
+function mainjl() :: Nothing
+  bin_folder_path = Genie.Generator.binfolderpath(pwd())
+  main_file_path = joinpath(bin_folder_path, "main.jl")
+
+  isfile(main_file_path) && error("File bin/main.jl already exists")
+
+  open(main_file_path, "w") do f
+    write(f, """
+push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
+
+ENV["STARTSERVER"] = true
+ENV["GENIE_ENV"] = "prod"
+ENV["EARLYBIND"] = true
+
+include("../bootstrap.jl")
+    """)
+  end
+
+  @info "File bin/main.jl created"
+
+  nothing
+end
 
 end
